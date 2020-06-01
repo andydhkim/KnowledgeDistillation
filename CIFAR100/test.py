a686bb811043e97e7c0d2e3b92c7e19669b8ebaf
Dong Hyun Kim <andydhkim97@gmail.com>
2020-06-01 00:02:12 -0400

    Updated training EOS and metrics

--- CIFAR100/test.py
+++ CIFAR100/test.py
@@ -12,6 +12,47 @@ from torch.autograd import Variable
 from helper import getModel, getTestloader
 from torchnet.meter import ConfusionMeter
 
+def compute_metrics(confusion_matrix):
+    sum_precision = 0
+    sum_recall = 0
+    sum_f1 = 0
+    for i in range(confusion_matrix.shape[0]):
+        tp = confusion_matrix[i, i]
+        tfp = np.sum(confusion_matrix[i, :])
+        tpfn = np.sum(confusion_matrix[:, [i]])
+
+        if tp == 0:
+            if tfp == 0 and tpfn == 0:
+                precision = 1
+                recall = 1
+                f1 = 1
+            else:
+                if tpfn == 0:
+                    precision = 1
+                    recall = 0
+                elif tfp == 0:
+                    precision = 0
+                    recall = 1
+                else:
+                    precision = 0
+                    recall = 0
+
+                f1 = 0
+        else:
+            precision = float(tp) / tfp
+            recall = float(tp) / tpfn
+            f1 = float(2 * precision * recall) / (precision + recall)
+
+        sum_precision += precision
+        sum_recall += recall
+        sum_f1 += f1
+
+    average_precision = float(sum_precision) / confusion_matrix.shape[0]
+    average_recall = float(sum_recall) / confusion_matrix.shape[0]
+    average_f1 = float(sum_f1) / confusion_matrix.shape[0]
+
+    return average_precision, average_recall, average_f1
+
 if __name__ == '__main__':
     # parse command line arguments
     parser = argparse.ArgumentParser()
@@ -31,6 +72,9 @@ if __name__ == '__main__':
     
     # obtain top1 and top 5 accuracy rate
     top1Acc, top5Acc = 0.0, 0.0
+    
+    # confusion matrix initialization
+    confusion_matrix = ConfusionMeter(100)
 
     # activation matrix
     activations = []
@@ -40,8 +84,13 @@ if __name__ == '__main__':
         image = Variable(image).cuda()
         label = Variable(label).cuda()
         output = model(image)
-
+        with torch.no_grad():
+            _, activation = model.forward(image, True)
+        
+        activations.append(activation)
         _, pred = output.topk(5, 1, largest=True, sorted=True)
+
+        confusion_matrix.add(output.data, label.data)
         
         label = label.view(label.size(0), -1).expand_as(pred)
         correct = pred.eq(label).float()
@@ -51,7 +100,22 @@ if __name__ == '__main__':
 
         #compute top1 
         top1Acc += correct[:, :1].sum()
+    
+    activations = torch.cat(activations)
+    activationSVD = torch.svd(activations)
+    S = activationSVD.S.cpu().numpy()
+    print(np.mean(S), np.std(S))
+    # create folder to save confusion matrix
+    if args.cm:
+        if not os.path.exists('confusion'):
+            os.makedirs('confusion')
+        confusionpath = os.path.join('confusion', args.model + '-' + args.state.split("/")[2] +'.txt')
+        np.savetxt(confusionpath, confusion_matrix.value().astype(int))
 
     print("Top 1 err: ", 1 - top1Acc / len(testloader.dataset))
     print("Top 5 err: ", 1 - top5Acc / len(testloader.dataset))
+    prec, recall, f1 = compute_metrics(confusion_matrix.value())
+    print("Precision: ", prec)
+    print("Recall: ", recall)
+    print("Macro F1: ", f1)
     print("Parameter numbers: {}".format(sum(p.numel() for p in model.parameters())))
